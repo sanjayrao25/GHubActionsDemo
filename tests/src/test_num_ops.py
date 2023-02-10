07c29af60b328ed5fc12026a19d34d6df8d7085e
sanjayrao25 <sanjayrao609@gmail.com>
2023-02-10 14:08:36 +0530

    Update test_num_ops.py

--- tests/src/test_num_ops.py
+++ tests/src/test_num_ops.py
@@ -1,13 +1,12 @@
-from num_ops import add, subtract, multiply, divide
 def test_add():
-  assert add(4, 5) == 9
+  assert (4 + 5) == 9
 
 def test_subtract():
-  assert subtract(5, 4) == 1
+  assert (5 - 4) == 1
 
 def test_multiply():
-  assert multiply(3, 4) == 12
+  assert (3 * 4) == 12
 
 def test_divide():
-  assert divide(4, 2) == 2
+  assert (4 // 2) == 2
    
